0739e0dc9b0c3994edbc1da9cd63951a19828fbc
vlrpor <v.forchkhidze@gmail.com>
2024-04-02 04:12:11 -0700

    add Primitive types and console log

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 let teamName = "Barca";
 const teamAge = 125;
 const teamPrice = null;
-let teamAvalaible = false;
+let teamAvailable = false;
 let teamBrand;
 
+console.log(teamName);
+console.log(teamAge);
+console.log(teamPrice);
+console.log(teamAvailable);
+console.log(teamBrand);
 
+let myName = "Valeri";
+let myHobby = "Swimming";
+const yearOfBirth = 1994;
+let currentYear = 2024;
+
+console.log(myName);
+console.log(myHobby);
+console.log(yearOfBirth);
+console.log(currentYear);
+
+let MyNameIs = myName;
+console.log(MyNameIs);
+let IAm = currentYear - yearOfBirth;
+console.log(IAm);
+let MyHobbyIs = myHobby;
+console.log(MyHobbyIs);
